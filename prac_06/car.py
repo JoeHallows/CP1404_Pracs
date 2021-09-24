713918a78dc7e1531777d8629de5b00ba6dd8976
Joe Hallows <joseph.hallows@my.jcu.edu.au>
2021-09-24 13:16:47 +1000

    Week 6 prac completed

--- prac_06/car.py
+++ prac_06/car.py
@@ -4,14 +4,14 @@
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, name="", fuel=0):
+    def __init__(self, name="", fuel=0, odometer=0):
         """Initialise a Car instance.
 
         fuel: float, one unit of fuel drives one kilometre
         """
         self.name = name
         self.fuel = fuel
-        self.odometer = 0
+        self.odometer = odometer
 
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
